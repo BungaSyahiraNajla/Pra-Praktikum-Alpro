3699a6f0f5f52d7e8c8fef4c6430470e5450e88e
BungaSyahiraNajla <101790324+BungaSyahiraNajla@users.noreply.github.com>
2022-03-18 11:58:01 +0700

    Update Prapraktikum.cpp

--- Prapraktikum.cpp
+++ Prapraktikum.cpp
@@ -17,7 +17,7 @@ int main ()
   	cout<<"Pilih angka	: ";
 	cin>>i;
 	   switch (i)
-	  {
+	  	{
 		   case 1 :
 			 {
 				 cout<<"Masukan angka 1 : "; cin>>a;
@@ -39,3 +39,26 @@ int main ()
 				 int z = e/f;
 				 cout<<"Hasilnya adalah : "; << z <<endl;
 			 }
+		   case 4 : 
+			 {
+			   	cout<<"Masukkan angka 1 : ";cin>>g;
+			   	cout<<"Masukkan angka 2 : ";cin>>h;
+			   	int r = g * h;
+			   	cout<<"Hasilnya adalah  :  "<<r<<endl;
+	   		}
+		   case 5 :
+			{
+				cout<<"Masukkab angka 	: ";cin>>j;
+				cout<<"Masukkan pangkat : ";cin>>k;
+				int m = pow(j,k);
+				cout<<"Hasilnya adalah  : "<<m<<endl;
+				break;
+			}
+		   case 6 :
+			{
+				cout<<"End"<<endl;
+			}
+		}
+		return 0;
+	}
+		
