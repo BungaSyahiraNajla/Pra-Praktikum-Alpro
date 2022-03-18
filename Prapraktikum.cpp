25c24848f4a225a4a13d3944e61e77304f8401d5
Indahdjen <101790313+Indahdjen@users.noreply.github.com>
2022-03-18 12:59:13 +0700

    Update Prapraktikum.cpp

--- Prapraktikum.cpp
+++ Prapraktikum.cpp
@@ -14,42 +14,47 @@ int main ()
 	cout<<"\t4. Perkalian"<<endl;
 	cout<<"\t5. Pangkat"<<endl;
 	cout<<"\t6. End"<<endl;
+	cout<<"========================================"<<endl;
   	cout<<"Pilih angka	: ";
 	cin>>i;
 	   switch (i)
 	  	{
 		   case 1 :
 			 {
-				 cout<<"Masukan angka 1 : "; cin>>a;
-				 cout<<"Masukan angka 2 : "; cin>>b;
+				 cout<<"Masukan Bilangan Pertama : "; cin>>a;
+				 cout<<"Masukan Bilangan Kedua   : "; cin>>b;
 				 int x = a + b;
-				 cout<<"Hasilnya adalah : "<< x <<endl;
+				 cout<<"Hasilnya adalah          : "<< x <<endl;
+				 break;
 			 }
 		   case 2 :
 			 {
-				 cout<<"Masukan angka 1 : "; cin>>c;
-				 cout<<"Masukan angka 2 : "; cin>>d;
+				 cout<<"Masukan Bilangan Pertama : "; cin>>c;
+				 cout<<"Masukan Bilangan Kedua   : "; cin>>d;
 				 int y = c - d;
-				 cout<<"Hasilnya adalah : "<< y <<endl;
+				 cout<<"Hasilnya adalah          : "<< y <<endl;
+				 break;
 			 }
 		   case 3 : 
 			 {
-				 cout<<"Masukan angka 1 : "; cin>>e;
-				 cout<<"Masukan angka 2 : "; cin>>f;
+				 cout<<"Masukan Bilangan Pertama : "; cin>>e;
+				 cout<<"Masukan Bilangan Kedua   : "; cin>>f;
 				 int z = e/f;
-				 cout<<"Hasilnya adalah : " << z <<endl;
+				 cout<<"Hasilnya adalah          : " << z <<endl;
+				 break;
 			 }
 		   case 4 : 
 			 {
-			   	cout<<"Masukkan angka 1 : "; cin>>g;
-			   	cout<<"Masukkan angka 2 : "; cin>>h;
+			   	cout<<"Masukkan Bilangan Pertama : "; cin>>g;
+			   	cout<<"Masukkan Bilangan Kedua   : "; cin>>h;
 			   	int r = g * h;
-			   	cout<<"Hasilnya adalah  :  "<< r <<endl;
+			   	cout<<"Hasilnya adalah           :  "<< r <<endl;
+				break;
 	   		}
 		   case 5 :
 			{
-				cout<<"Masukkab angka 	: "; cin>>j;
-				cout<<"Masukkan pangkat : "; cin>>k;
+				cout<<"Masukkan Bilangan Pertama : "; cin>>j;
+				cout<<"Masukkan Bilangan Pangkat : "; cin>>k;
 				int m = pow(j,k);
 				cout<<"Hasilnya adalah  : "<< m <<endl;
 				break;
