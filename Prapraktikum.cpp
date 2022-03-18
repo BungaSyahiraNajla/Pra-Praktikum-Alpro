269a4659f9dc497cb17c18e703a23331413f539d
Ghaitsa Zahira Rahma <101691351+ghaitsazahirar@users.noreply.github.com>
2022-03-18 11:29:06 +0700

    mengisi menu pemilihan sampai ke switch

--- Prapraktikum.cpp
+++ Prapraktikum.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
 
 int main ()
 {
+  int a, b, c, d, e, f, g, h, i, j, k;
+	cout<<"========================================"<<endl;
+	cout<<"\tKALKULATOR SEDERHANA"<<endl;
+	cout<<"========================================"<<endl;
+	cout<<"\t1. Penambahan"<<endl;
+	cout<<"\t2. Pengurangan"<<endl;
+	cout<<"\t3. Pembagian"<<endl;
+	cout<<"\t4. Perkalian"<<endl;
+	cout<<"\t5. Pangkat"<<endl;
+	cout<<"\t6. End"<<endl;
+  cout<<"Pilih angka	: ";
+	cin>>i;
+	   switch (i)
+	  {
