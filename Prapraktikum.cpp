2d3d650352b36f875ca21c618dbdd550aa009d21
Indahdjen <101790313+Indahdjen@users.noreply.github.com>
2022-03-18 11:39:52 +0700

    Update Prapraktikum.cpp

--- Prapraktikum.cpp
+++ Prapraktikum.cpp
@@ -14,7 +14,28 @@ int main ()
 	cout<<"\t4. Perkalian"<<endl;
 	cout<<"\t5. Pangkat"<<endl;
 	cout<<"\t6. End"<<endl;
-  cout<<"Pilih angka	: ";
+  	cout<<"Pilih angka	: ";
 	cin>>i;
 	   switch (i)
 	  {
+		   case 1 :
+			 {
+				 cout<<"Masukan angka 1 : "; cin>>a;
+				 cout<<"Masukan angka 2 : "; cin>>b;
+				 int x = a + b;
+				 cout<<"Hasilnya adalah : "<< x <<endl;
+			 }
+		   case 2 :
+			 {
+				 cout<<"Masukan angka 1 : "; cin>>c;
+				 cout<<"Masukan angka 2 : "; cin>>d;
+				 int y = c - d;
+				 cout<<"Hasilnya adalah : "<< y <<endl;
+			 }
+		   case 3 : 
+			 {
+				 cout<<"Masukan angka 1 : "; cin>>e;
+				 cout<<"Masukan angka 2 : "; cin>>f;
+				 int z = e/f;
+				 cout<<"Hasilnya adalah : "; << z <<endl;
+			 }
